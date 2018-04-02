da560ee30923aa544ef6868a6857df6b196fd087
Yaswanth Sai Palaghat <yashu827284@gmail.com>
2018-04-02 21:34:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ function calculate()
+            {
+                p = document.getElementById("p").value;
+                n = document.getElementById("n").value;
+                r = document.getElementById("r").value;
+                result = document.getElementById("result");
+                
+                result.innerHTML = "The interest is " + (p*n*r/100);
+            }
